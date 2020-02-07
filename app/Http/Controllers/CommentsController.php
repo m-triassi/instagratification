0bf766e1bdede60263b26f67aa2f21a4bff144f6
Massimo Triassi <massimo@plankdesign.com>
2020-02-07 16:07:38 -0500

    feat: add comment controller and create comment func, add comment model relations

---
+++ app/Http/Controllers/CommentsController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Comment;
+use App\Models\Post;
+use Illuminate\Support\Facades\Request;
+
+class CommentsController extends Controller
+{
+    public function create(Request $request)
+    {
+        $content = $request->comment;
+        $author = $request->author;
+        $post = $request->postID;
+
+        $comment = new Comment();
+        $comment->comment = $content;
+        $comment->post()->associate($post);
+        $comment->author()->associate($author);
+
+        $comment->save();
+
+    }
+}
