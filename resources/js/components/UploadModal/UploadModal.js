80d4b4b22acbee190eb79b75aafcf8e1e9979191
Wilson <wilsonhuang00815@gmail.com>
2020-02-12 13:42:09 -0500

    feat: connect like and comment to backend from photo component

---
+++ resources/js/components/UploadModal/UploadModal.js
@@ -0,0 +1,5 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+import { Modal, Form } from 'antd'
+import { postPhoto } from '../services/index'
+
--- resources/js/components/index.js
+++ resources/js/components/index.js
@@ -1,2 +1,1 @@
-export { default as Example } from './Example/Example'
 export { default as Photo } from './Photo/Photo'
