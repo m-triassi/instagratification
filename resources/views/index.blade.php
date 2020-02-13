dac230ce21de511d7f6edd4ba9f7c26d98eadf5e
Ramsha <ramshalashari@gmail.com>
2020-02-13 13:05:08 -0500

    Fix nagivation bar title/icon. Using temp icon for testing only.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Instagratification</title>
+        <link rel="icon" href="iconTest.ico" type="image/x-icon">
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,9 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Instagratification</title>
+    <title>{{ config('app.name', 'Instagratification') }}</title>
+    <link rel="icon" href="iconTest.ico" type="image/x-icon">
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
