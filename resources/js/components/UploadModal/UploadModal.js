3871ef17614a2292301444649b010c112f10cd70
Wilson <wilsonhuang00815@gmail.com>
2020-02-14 18:35:06 -0500

    feat: update upload modal

--- resources/js/components/UploadModal/UploadModal.js
+++ resources/js/components/UploadModal/UploadModal.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 import {
-  Button, Icon, Modal, message, Row, Input, Upload,
+  Button, Form, Icon, Modal, message, Row, Input, Upload,
 } from 'antd'
 import PropTypes from 'prop-types'
 import { postPicture } from '../services/index'
@@ -11,14 +11,23 @@ const UploadModal = (props) => {
   const [caption, setCaption] = useState('')
   const [upload, setUpload] = useState([])
   const handlePost = () => {
-    postPicture({ caption, media: upload.originFileObj, author: props.authorId }).then((response) => {
-      if (response.data.success) {
-        message.success('Post Uploaded')
-      }
-      setIsModalVisible(false)
-    })
+    if (!upload || !caption) {
+      message.error('post failed')
+    }
+    postPicture({ caption, media: upload, author: props.authorId })
+      .then((response) => {
+        if (response.data.success) {
+          message.success('post uploaded')
+        } else message.error('post failed')
+        setIsModalVisible(false)
+      })
   }
 
+  const dummyRequest = ({ onSuccess }) => {
+    setTimeout(() => {
+      onSuccess('ok')
+    }, 0)
+  }
   return (
     <Row>
       {isModalVisible && (
@@ -27,18 +36,28 @@ const UploadModal = (props) => {
           visible={isModalVisible}
           onCancel={() => setIsModalVisible(false)}
           onOk={handlePost}>
-          <Upload
-            accept='image/*'
-            customRequest={() => { }}
-            onChange={(value) => setUpload(value.file)}>
-            <Button>
+          <Form.Item label='Upload' required>
+            <Upload
+              accept='image/*'
+              customRequest={dummyRequest}
+              onChange={(value) => {
+                const reader = new FileReader()
+                reader.onloadend = () => {
+                  setUpload(reader.result)
+                }
+                reader.readAsDataURL(value.file.originFileObj)
+              }}>
+              <Button icon='upload'>
               click here to Upload
-            </Button>
-          </Upload>
-          <Input
-            value={caption}
-            onChange={(value) => setCaption(value.target.value)}
-            placeholder='enter a caption here' />
+              </Button>
+            </Upload>
+          </Form.Item>
+          <Form.Item label='Caption' required>
+            <Input
+              value={caption}
+              onChange={(value) => setCaption(value.target.value)}
+              placeholder='enter a caption here' />
+          </Form.Item>
         </Modal>)}
       <Icon type='plus' onClick={() => setIsModalVisible(true)} />
     </Row>
