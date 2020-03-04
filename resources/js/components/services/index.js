3adb3b2750d82dcaa7c028d3eebb3bba853440a0
Massimo Triassi <massimo@plankdesign.com>
2020-03-03 20:30:41 -0500

    fix: change routes in JS service index to absolute instead of relative

--- resources/js/components/services/index.js
+++ resources/js/components/services/index.js
@@ -2,14 +2,14 @@ import axios from 'axios'
 
 // Internal API post requests
 export async function postPicture(payload) {
-    return axios.post('post/create', payload)
+    return axios.post('/post/create', payload)
 }
 
 export async function likePhoto(payload) {
-    return axios.post('post/like', payload)
+    return axios.post('/post/like', payload)
 }
 
 export async function commentPhoto(payload) {
-    return axios.post('comment/create', payload)
+    return axios.post('/comment/create', payload)
 }
 
