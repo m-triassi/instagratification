78dc636a82d58ef31626e49fa3c173141cfc7e39
Ramsha <Ramshalashari@gmail.com>
2020-03-04 20:12:54 -0500

    Favicon updated
    Added the favicon icon

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
             <div id='photo-container' class='container'>.
         <title>Instagratification</title>
-        <link rel="icon" href="iconTest.ico" type="image/x-icon">
+        <link rel="icon" href="favicon.ico" type="image/x-icon">
             @foreach($posts as $i => $post)
                 <div class="photo" id="post_{{ $i }}" post="{{ $post }}" user="{{ $loggedInUser ?? Auth::user() }}" />
             @endforeach
