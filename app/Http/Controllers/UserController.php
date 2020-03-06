078baaa84d1178b94c9667eca77f5836096a91c0
Massimo Triassi <massimo@plankdesign.com>
2020-03-06 11:41:44 -0500

    fix: #74 remove commented code

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -18,8 +18,7 @@ class UserController extends Controller
 
         $loggedInUser = Auth::user();
         $userCanFollow = !($loggedInUser->following()->get()->contains($user->id));
-        //$posts = Post::where('author_id', $loggedInUser->id)->get();
-
+        
         return view('user.view')->with(compact(['user', 'loggedInUser', 'userCanFollow']));
     }
 
